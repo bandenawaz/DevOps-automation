9ad4d36892508d9c94b803aec70b750f4d08b59a
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-16 07:23:49 +0530

    first commit

---
+++ src/test/java/com/illuminateskills/spring/devops_integration/api/DevopsItegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illuminateskills.spring.devops_integration.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsItegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
